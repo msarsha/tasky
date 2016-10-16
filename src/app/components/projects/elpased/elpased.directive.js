9ca736b72cd44ee92d439e7dae4fdaf6979820da
Matan SarSha <smatan84@gmail.com>
2016-10-16 15:35:31 +0300

    project style

--- src/app/components/projects/elpased/elpased.directive.js
+++ src/app/components/projects/elpased/elpased.directive.js
@@ -5,11 +5,20 @@ var elpased = function ($interval) {
 
     function updateElpasedTime() {
       var passed_time = new Date().getTime() - scope.date;
-      var sec_m;
+      var sec_m, sec, min, hour;
       sec_m = passed_time / 1000;
-      scope.sec = Math.floor(sec_m % 60);
-      scope.min = Math.floor((sec_m / 60) % 60);
-      scope.hour = Math.floor((sec_m / 60 / 60) % 60);
+
+      sec = Math.floor(sec_m % 60);
+      min = Math.floor((sec_m / 60) % 60);
+      hour = Math.floor((sec_m / 60 / 60) % 60);
+
+      scope.sec = pad(sec);
+      scope.min = pad(min);
+      scope.hour = pad(hour);
+    }
+
+    function pad(value) {
+      return value.toString().length < 2 ? "0" + value : value;
     }
 
     elm.on('$destroy', function () {
