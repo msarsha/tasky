41c91a1ac1be088b3578cde028a0af83120bdd4d
Matan SarSha <smatan84@gmail.com>
2016-11-06 13:58:43 +0200

    min/max date workaround

--- src/app/components/auth/login/login.controller.js
+++ src/app/components/auth/login/login.controller.js
@@ -1,18 +1,21 @@
 function LoginController(authService, $state, spinnerService) {
   this.title = "Login Component";
 
-  this.onLogin = function($event){
+  this.onLogin = function ($event) {
     spinnerService.show('app-spinner');
     authService
       .login($event.user)
-      .then(function(){
+      .then(function () {
         console.log('logged in');
         spinnerService.closeAll();
         $state.go('app');
       })
+      .finally(function () {
+        spinnerService.closeAll();
+      })
   }
 }
 
-angular 
+angular
   .module('auth')
   .controller('LoginController', LoginController);
